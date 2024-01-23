7af2727cad2a0c45b2cc37649d5615ee44c85251
Vipin Goand <109461704+vipinGoand123@users.noreply.github.com>
2024-01-23 13:29:50 +0000

    lecture2

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 144553;
+let accountEmail = "vipingoand99@gmail.com";
+var accountPassword = "123456";
+accountCity = "Lucknow";
+
+let accountState;
+
+
+// accountId = 2;  // Not allowed
+
+accountEmail = "vipingoand96@gmail.com";
+accountPassword = "654321";
+accountCity = "Azamgarh"
+
+console.log(accountId);
+
+/*
+Prefer not use var 
+because of issue in blog scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,9 @@
+"use strict"; // Treat all JS Code as newer version
+
+// alert(3 + 3);  // We are using nodejs, not browser
+
+console.log(3+3);
+
+let name = "Vipin";
+let age = 24;
+let isLoggedIn = false;
