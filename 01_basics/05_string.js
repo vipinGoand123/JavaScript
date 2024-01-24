ec38e579bd54c2b4388f716411ff67897743136b
Vipin Goand <109461704+vipinGoand123@users.noreply.github.com>
2024-01-24 09:48:43 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,41 @@
+const name = "Vipin";
+
+const repoCunt = 4;
+
+// console.log(name + repoCunt + " Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCunt}`);
+
+
+const gameName = new String ('Vipin Goand');
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('i'));
+
+
+const neString  = gameName.substring(0,4);
+// console.log(neString);
+
+const anotherString = gameName.slice(-11,4);
+// console.log(anotherString)
+
+const newStringOne = "    Vipin   ";
+
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+
+
+
+const url = "https://vipin%20goand";
+
+// console.log(url.replace('%20', '-'));
+
+// console.log(url.includes('goand'));
+
+
+console.log(gameName.split('-'));
