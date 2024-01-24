df1c4deb057f78df7835ff695c55255c3b27ebfa
Vipin Goand <109461704+vipinGoand123@users.noreply.github.com>
2024-01-24 07:39:51 +0000

    data_types

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -7,3 +7,22 @@ console.log(3+3);
 let name = "Vipin";
 let age = 24;
 let isLoggedIn = false;
+
+/*
+number => 2 to power 53
+bigint
+string =>""
+boolean => true/false
+null => standalone value
+undefined =>
+symblol => unique
+
+
+
+Object 
+
+*/
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // Object
