03d6608d004999ccfbd160e210ed22701004153b
Vipin Goand <109461704+vipinGoand123@users.noreply.github.com>
2024-01-24 08:41:07 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,37 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+
+
+
+// console.log("2">1)
+// console.log("02">1)
+
+
+
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)
+
+
+
+// console.log(undefined == 0)
+// console.log(undefined >= 0)
+// console.log(undefined > 0)
+
+
+
+// console.log(undefined>null)
+// console.log(undefined==null)
+// console.log(undefined>=null)
+
+
+
+// console.log(null>undefined)
+// console.log(null==undefined)
+// console.log(null>=undefined)
+
+
+// console.log("2" === 2)
