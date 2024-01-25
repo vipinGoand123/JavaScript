6b2f21eb726eb0a58837f8bb57cdeadd5b75ff3d
Vipin Goand <109461704+vipinGoand123@users.noreply.github.com>
2024-01-25 07:54:58 +0000

    Object3

---
+++ 02_basics/05_object.js
@@ -0,0 +1,21 @@
+const course = {
+    courseName: "Js in Hindi",
+    price: "999",
+    courseInstructor: "Hitesh"
+}
+
+
+// console.log(course.courseInstructor)
+
+
+const {courseInstructor:i, price:p, courseName:c} = course
+
+// console.log(i)
+// console.log(p)
+// console.log(c)
+
+// {
+//     "courseName" : "Js in Hindi",
+//     "price" : "999",
+//     "courseInstructor" : "Hitesh"
+// }
