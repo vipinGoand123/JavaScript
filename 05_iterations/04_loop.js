81f2361ba436c1e420bf6e298285cc95dc2257b3
Vipin Goand <109461704+vipinGoand123@users.noreply.github.com>
2024-01-30 07:29:13 +0000

    lectur 4 loop

---
+++ 05_iterations/04_loop.js
@@ -0,0 +1,28 @@
+const myObject = {
+    js: 'javaScript',
+    cpp: 'C++',
+    rb: 'Ruby',
+    swif: 'Swift by apple'
+}
+
+for (const key in myObject) {
+    // console.log(`${key} Shortcut is for ${myObject[key]}`)
+}
+
+
+const programming = ['js', 'ruby','c++', 'java','python']
+
+for (const key in programming) {
+//    console.log(programming[key]);
+}
+
+
+// const map = new Map()
+// map.set('IN', "India");
+// map.set('USA', "United States of America");
+// map.set('Fr', "France");
+// map.set('IN', "India");
+
+// for (const key in map) {
+//     console.log(key)
+// }
