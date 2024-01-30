686420d948603aafef7997d25591c58dfbad894d
Vipin Goand <109461704+vipinGoand123@users.noreply.github.com>
2024-01-30 05:57:12 +0000

    if else

--- 04_control_flow/01_if_else.js
+++ 04_control_flow/01_if_else.js
@@ -50,3 +50,28 @@ if (userLoggedIn && debitCard && 2===3) {
 if (loggedInFromGoogle || loogedInFromEmail) {
     console.log("User logged in")
 }
+
+
+// NUllish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10;
+
+// val1 = null ?? 10;
+
+// val1 = undefined ?? 15;
+
+val1 = null ?? 10 ?? 20
+
+console.log(val1)
+
+
+
+// Terniary Operator
+
+
+// condition ? true : false
+
+const iceTeaPrice = 100;
+
+iceTeaPrice <=80 ? console.log("less than 80"):console.log("More than 80");
