e568d765e7647e03a7549f10f9dc8da123db8b97
Vipin Goand <109461704+vipinGoand123@users.noreply.github.com>
2024-02-02 11:19:19 +0000

    0bject set get

--- 10_classes_and_oop/object_get_set.js
+++ 10_classes_and_oop/object_get_set.js
@@ -1,26 +1,24 @@
-
 const User = {
-    _email: 'v@v.com',
-    _password: 'abcvipin',
+  _email: "v@v.com",
+  _password: "abcvipin",
 
-    get email(){
-        return this._email.toUpperCase();
-    },
+  get email() {
+    return this._email.toUpperCase();
+  },
 
-    set email(value){
-        this._email = value
-    },
+  set email(value) {
+    this._email = value;
+  },
 
-    get password(){
-        return this._password.toUpperCase();
-    },
+  get password() {
+    return this._password.toUpperCase();
+  },
 
-    set password(value){
-        this._password = value
-    }
-}
+  set password(value) {
+    this._password = value;
+  },
+};
 
-
-const tea = Object.create(User)
+const tea = Object.create(User);
 
 console.log(tea.email);
